92df5777a9c2608475ae7aa10ddb363ed64efc6c
dhrvrm <48381412+dhrvrm@users.noreply.github.com>
2024-01-06 21:53:19 +0530

    Font added

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,16 +1,14 @@
-import './directory.styles.scss'
+import "./directory.styles.scss";
 import CategoryItem from "../category-item/category-item.component";
 
-const Directory = ({categories}) => {
-  
-
+const Directory = ({ categories }) => {
   return (
     <div className="directory-container">
       {categories.map((category) => (
-        <CategoryItem category={category} key={category.id}/>
+        <CategoryItem category={category} key={category.id} />
       ))}
     </div>
   );
-}
+};
 
-export default Directory
+export default Directory;
