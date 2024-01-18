f224f128f090c33d797ec80e113d7304b3d469ec
dhrvrm <48381412+dhrvrm@users.noreply.github.com>
2024-01-18 23:52:40 +0530

    Add User Store

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
 
-import { UserProvider } from "./context/user.context";
+import App from "./App";
 import { CategoriesProvider } from "./context/categories.context";
 import { CartProvider } from "./context/cart.context";
 
-import App from "./App";
+import { store } from "./store/store";
+
 import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
+    <Provider store={store}>
+      <BrowserRouter>
         <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
         </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
