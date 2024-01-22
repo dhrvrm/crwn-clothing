2441164bc0f94393df70ec96337dcc0527074687
dhrvrm <48381412+dhrvrm@users.noreply.github.com>
2024-01-22 20:49:37 +0530

    Cart Icon Redux

--- src/store/cart/cart.selector.js
+++ src/store/cart/cart.selector.js
@@ -1,22 +1,24 @@
 import { createSelector } from "reselect";
 
-export const selectCartReducer = (state) => state.cart;
+const selectCartReducer = (state) => state.cart;
 
-export const selectCartItems = createSelector(
-  [selectCartReducer],
-  (cart) => cart.items
-);
-
-export const isCartOpen = createSelector(
+export const selectIsCartOpen = createSelector(
   [selectCartReducer],
   (cart) => cart.isCartOpen
 );
 
-const newCartCount = newCartItems.reduce(
-  (total, cartItem) => total + cartItem.quantity,
-  0
+export const selectCartItems = createSelector(
+  [selectCartReducer],
+  (cart) => cart.cartItems
 );
-const newCartTotal = newCartItems.reduce(
-  (total, cartItem) => total + cartItem.quantity * cartItem.price,
-  0
+
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce(
+    (total, cartItem) => total + cartItem.quantity * cartItem.price,
+    0
+  )
+);
+
+export const selectCartCount = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
 );
